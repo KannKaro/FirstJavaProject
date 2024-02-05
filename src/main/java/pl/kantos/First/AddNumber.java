66e236c81f1f5405bad59c6cef7012e3e5f503d4
KannKaro <kacpersemman@gmail.com>
2024-02-05 14:48:27 +0100

    d

--- src/main/java/pl/kantos/First/AddNumber.java
+++ src/main/java/pl/kantos/First/AddNumber.java
@@ -2,11 +2,12 @@ package pl.kantos.First;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class AddNumber {
     @GetMapping("/test")
-    public int AddNum() {
-        return 1 + 2;
+    public int AddNum(@RequestParam("n1") int n1, @RequestParam("n2") int n2) {
+        return n1 + n2;
     }
 }
