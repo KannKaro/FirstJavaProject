3ae19614ed886605fcb38f9d9b806b3cb237f828
KannKaro <kacpersemman@gmail.com>
2024-02-05 18:37:37 +0100

    more renames

--- src/main/java/pl/kantos/first/AddNumber.java
+++ src/main/java/pl/kantos/first/AddNumber.java
@@ -10,12 +10,12 @@ import java.math.BigDecimal;
 
 @RestController
 public class AddNumber {
-    @GetMapping("/AddNumbers")
+    @GetMapping("/addNumbers")
     public BigDecimal sumNumbers(@RequestParam("number1") BigDecimal number1, @RequestParam("number2") BigDecimal number2) {
         return number1.add(number2);
     }
 
-    @PostMapping("/AddNumbersPost")
+    @PostMapping("/addNumbersPost")
     public BigDecimal sumOfNumbersPostMethod(@RequestBody NumbersPostMethod numbersPostMethod) {
         BigDecimal number1 = numbersPostMethod.createNumber1();
         BigDecimal number2 = numbersPostMethod.createNumber2();
