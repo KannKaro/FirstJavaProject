06a4d678ae0a616049bfd22e8d7044d65355de76
KannKaro <kacpersemman@gmail.com>
2024-02-05 18:29:17 +0100

    added Post method

--- src/main/java/pl/kantos/first/AddNumber.java
+++ src/main/java/pl/kantos/first/AddNumber.java
@@ -1,16 +1,24 @@
 package pl.kantos.first;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.math.BigDecimal;
 
 @RestController
 public class AddNumber {
     @GetMapping("/AddNumbers")
-    public BigDecimal addNum(@RequestParam("number1") @NotNull BigDecimal number1, @RequestParam("number2") BigDecimal number2) {
+    public BigDecimal addNum(@RequestParam("number1") BigDecimal number1, @RequestParam("number2") BigDecimal number2) {
+        return number1.add(number2);
+    }
+
+    @PostMapping("/AddNumbersPost")
+    public BigDecimal addNumbers(@RequestBody NumbersPostMethod numbers) {
+        BigDecimal number1 = numbers.createNumber1();
+        BigDecimal number2 = numbers.createNumber2();
         return number1.add(number2);
     }
 }
---
+++ src/main/java/pl/kantos/first/NumbersPostMethod.java
@@ -0,0 +1,18 @@
+package pl.kantos.first;
+
+import java.math.BigDecimal;
+
+public class NumbersPostMethod {
+    public BigDecimal number1;
+    public BigDecimal number2;
+
+    // Getters and setters
+    public BigDecimal createNumber1() {
+        return number1;
+    }
+
+    public BigDecimal createNumber2() {
+        return number2;
+    }
+
+}
