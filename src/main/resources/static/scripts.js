1c80ddc3bb775cb54574b17852ef078de1782957
KannKaro <kacpersemman@gmail.com>
2024-02-06 14:33:36 +0100

    working interface added

---
+++ src/main/resources/static/scripts.js
@@ -0,0 +1,41 @@
+function addNumbersPOST() {
+    let number1 = document.getElementById('number1').value 
+    let number2 = document.getElementById('number2').value
+    
+    fetch('/addNumbersPOST', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            number1: number1,
+            number2: number2
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        }
+        return response.json()
+    })
+    .then(data => {
+        document.getElementById("result").innerText = "Result: " + data
+    })
+}
+
+function addNumbersGET() {
+    let number1 = document.getElementById('number1').value 
+    let number2 = document.getElementById('number2').value
+
+    fetch('/addNumbers?number1=' + number1 + '&number2=' + number2)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json()
+    })
+    .then(data => {
+     document.getElementById("result").innerText = "Result: " + data;
+    })
+}
+
