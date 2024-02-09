170f790c8ea5aef900d8114af0289bf7a51805a5
KannKaro <kacpersemman@gmail.com>
2024-02-09 14:39:05 +0100

    created RestTemplate Config

--- src/main/java/pl/kantos/first/WordController.java
+++ src/main/java/pl/kantos/first/WordController.java
@@ -1,5 +1,6 @@
 package pl.kantos.first;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -7,19 +8,22 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/words")
 public class WordController {
+    public RestTemplate restTemplate;
     @Value("${wordApi.address}")
     public String wordApiAddress;
+    @Autowired
+    public WordController(RestTemplate restTemplate){
+        this.restTemplate = restTemplate;
+    }
 
     @GetMapping("/")
     public String wordFromSite(@RequestParam("amountOfWords") int amount) {
-        RestTemplate restTemplate = new RestTemplate();
         String url = wordApiAddress + "/word?number=" + amount;
         return restTemplate.getForObject(url, String.class);
     }
 
     @PostMapping("/")
     public String wordFromSiteSecondOption(@RequestBody AmountOfWordsRequest request) {
-        RestTemplate restTemplate = new RestTemplate();
         String url = wordApiAddress + "/word?number=" + request.amountOfWords();
         return restTemplate.getForObject(url, String.class);
     }
